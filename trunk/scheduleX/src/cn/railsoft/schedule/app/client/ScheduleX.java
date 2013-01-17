b4aac8239ca4ada85b3cb2e373bcb0178a72d50b
ebiskit@gmail.com <ebiskit@gmail.com@adcd3718-2b03-21e4-ee39-54743e69689c>
2013-01-17 07:26:33 +0000

    improve:分离用于about注册的提交程序

--- trunk/scheduleX/src/cn/railsoft/schedule/app/client/ScheduleX.java
+++ trunk/scheduleX/src/cn/railsoft/schedule/app/client/ScheduleX.java
@@ -2,158 +2,31 @@ package cn.railsoft.schedule.app.client;
 
 import java.util.ArrayList;
 
-import cn.railsoft.schedule.app.shared.FieldVerifier;
 import cn.railsoft.schedule.app.shared.Jobitem;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.DockPanel;
-import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.Grid;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.ScrollPanel;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.TabPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class ScheduleX implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
 
 	/**
 	 * Create a remote service proxy to talk to the server-side Greeting service.
 	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
+	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("关于");
-		final TextBox nameField = new TextBox();
-		nameField.setText("软件");
-		final Label errorLabel = new Label();
-
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("关闭");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
-		
 		/**
 		 * 刘辉的代码2013/01/16
 		 */
@@ -169,9 +42,9 @@ public class ScheduleX implements EntryPoint {
 			public void onSuccess(ArrayList<Jobitem> result) {
 				// TODO Auto-generated method stub
 				if (result!=null){
-					nameField.setText("+"+result.get(0).getId());
+					//nameField.setText("+"+result.get(0).getId());
 				}else{
-					nameField.setText("getJobItemList_null");
+					//nameField.setText("getJobItemList_null");
 				}
 			}
 		});
@@ -180,36 +53,17 @@ public class ScheduleX implements EntryPoint {
 		dp.setSize("800px", "600px");
 		
 		HorizontalPanel horizontalPanel = new HorizontalPanel();
-		horizontalPanel.setSpacing(1);
+		horizontalPanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
 		horizontalPanel.setSize("800px", "30px");
-		horizontalPanel.add(nameField);
-		horizontalPanel.add(sendButton);
-		horizontalPanel.add(errorLabel);
+		horizontalPanel.add(new About(greetingService));
+//		horizontalPanel.add(nameField);
+//		horizontalPanel.add(sendButton);
+//		horizontalPanel.add(errorLabel);
 		dp.add(horizontalPanel, DockPanel.NORTH);
 		
 		TabPanel tabPanel = new TabPanel();
 		tabPanel.setSize("800px", "600px");
 		dp.add(tabPanel, DockPanel.CENTER);
-		
-		SimplePanel simplePanel = new SimplePanel();
-		simplePanel.setSize("800px", "600px");
-		tabPanel.add(simplePanel, "Calendar1", false);
-		
-		FlexTable flexTable = new FlexTable();
-		tabPanel.add(flexTable, "Calendar2", false);
-		flexTable.setSize("800px", "600px");
-		
-		Label lblNewLabel = new Label("New label");
-		flexTable.setWidget(0, 0, lblNewLabel);
-		
-		ScrollPanel scrollPanel = new ScrollPanel();
-		tabPanel.add(scrollPanel, "New tab", false);
-		scrollPanel.setSize("5cm", "3cm");
-		
-		Grid grid = new Grid(10, 10);
-		scrollPanel.setWidget(grid);
-		scrollPanel.setSize("800px", "600px");
-		grid.setSize("100%", "100%");
 		
 		RootPanel.get("gwtwindow").add(dp);
 	}
