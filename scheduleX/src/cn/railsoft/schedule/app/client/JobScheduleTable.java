304daef73b4dbe98a89961e17e50c4d35c10389d
ebiskit <ebiskit@gmail.com>
2013-01-17 13:40:49 +0000

    IMPROVE

--- scheduleX/src/cn/railsoft/schedule/app/client/JobScheduleTable.java
+++ scheduleX/src/cn/railsoft/schedule/app/client/JobScheduleTable.java
@@ -1,25 +1,80 @@
 package cn.railsoft.schedule.app.client;
 
+import java.util.ArrayList;
+
+import cn.railsoft.schedule.app.shared.Jobitem;
+
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Grid;
 
 public class JobScheduleTable extends Composite {
 
+	GreetingServiceAsync greetingService = null;
 	private final NavBar navbar = new NavBar();
+	private int irows = 1;
+	private int icolumns = 20;
+	private ArrayList<Jobitem> jobitemlist = null;
+	private Grid grid = null;
 
-	public JobScheduleTable() {
+	public JobScheduleTable(GreetingServiceAsync greetingService) {
+		this.greetingService = greetingService;
 		DockPanel dockPanel = new DockPanel();
 		dockPanel.setStyleName("jobScheduleTable");
 		initWidget(dockPanel);
 		dockPanel.add(navbar, DockPanel.NORTH);
+		grid = new Grid(irows, icolumns);
+		dockPanel.add(grid, DockPanel.CENTER);
+		// grid.resize(10, 10);
+		// grid.setText(5, 5, "我的测试数据");
+		//loadJobScheduleData();
 	}
 
+	public void loadJobSchedule() {
+		greetingService.getJobItemList(new AsyncCallback<ArrayList<Jobitem>>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				
+			}
+
+			@Override
+			public void onSuccess(ArrayList<Jobitem> result) {
+				if (result != null) {
+					jobitemlist = result;
+					// nameField.setText("+"+result.get(0).getId());
+				} else {
+					// nameField.setText("getJobItemList_null");
+				}
+				loadJobScheduleData();
+			}
+		});
+
+	}
+
+	private void loadJobScheduleData(){
+		if (jobitemlist!=null){
+			irows = jobitemlist.size();
+			grid.resize(irows + 1, icolumns);
+			for (int i = 0; i < irows; i++) {
+				grid.getCellFormatter().setStyleName(i+1, 1, "tablecell");
+				grid.setText(i + 1, 0, jobitemlist.get(i).getId());
+				grid.setText(i + 1, 1, jobitemlist.get(i).getContent1());
+				grid.setText(i + 1, 2, jobitemlist.get(i).getContent2());
+			}
+		}else{
+			grid.resize(1, icolumns);
+		}
+		grid.setText(0, 0, "序号");
+		grid.setText(0, 1, "2");
+	}
+	
 	private class NavBar extends Composite implements ClickHandler {
 		public final DockPanel bar = new DockPanel();
 		public final Button gotoFirst = new Button("&lt;&lt;", this);
--- scheduleX/src/cn/railsoft/schedule/app/client/ScheduleX.java
+++ scheduleX/src/cn/railsoft/schedule/app/client/ScheduleX.java
@@ -1,12 +1,7 @@
 package cn.railsoft.schedule.app.client;
 
-import java.util.ArrayList;
-
-import cn.railsoft.schedule.app.shared.Jobitem;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -17,27 +12,10 @@ public class ScheduleX implements EntryPoint {
 	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
 	public void onModuleLoad() {
-		/**
-		 * 刘辉的代码2013/01/16
-		 */
-		greetingService.getJobItemList(new AsyncCallback<ArrayList<Jobitem>>() {
-			@Override
-			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
-			}
-			@Override
-			public void onSuccess(ArrayList<Jobitem> result) {
-				// TODO Auto-generated method stub
-				if (result!=null){
-					//nameField.setText("+"+result.get(0).getId());
-				}else{
-					//nameField.setText("getJobItemList_null");
-				}
-			}
-		});
 
-		JobScheduleTable jobScheduleTable = new JobScheduleTable();
+		JobScheduleTable jobScheduleTable = new JobScheduleTable(greetingService);
 		jobScheduleTable.setSize("800px", "500px");
+		jobScheduleTable.loadJobSchedule();
 		
 		AbsolutePanel absolutePanel = new AbsolutePanel();
 		absolutePanel.setSize("800px", "30px");
