f1e6acc9bbdd951cec4dfdb1779ca1b11db026c6
ebiskit@gmail.com <ebiskit@gmail.com@adcd3718-2b03-21e4-ee39-54743e69689c>
2014-02-07 03:55:01 +0000

    ADD:实现本地展示查询!

--- trunk/jobassistant2/src/org/job/task/Boot.java
+++ trunk/jobassistant2/src/org/job/task/Boot.java
@@ -1,5 +1,7 @@
 package org.job.task;
 
+import org.job.ui.Mainwin;
+
 public class Boot {
 
 	public static void main(String[] args) {
@@ -8,9 +10,11 @@ public class Boot {
 		//2.启动界面，加载数据库中默认的数据取出；
 		
 		//3.启动轮训进程从网页上面取得数据；
-		
+		new Thread(new CrawlerProcess()).start();
 		//4.通知界面要进行轮训；禁用
-		
 
+		//5.启动界面; 
+		Mainwin window = new Mainwin();
+		window.open();
 	}
 }
