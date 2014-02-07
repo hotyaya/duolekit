9d63c7ee2e1751df8f8210b2fbe750457a15c563
ebiskit@gmail.com <ebiskit@gmail.com@adcd3718-2b03-21e4-ee39-54743e69689c>
2014-02-07 16:01:16 +0000

    BUG:修正JFTP的使用！

--- trunk/jobassistant2/src/org/job/task/Boot.java
+++ trunk/jobassistant2/src/org/job/task/Boot.java
@@ -3,10 +3,10 @@ package org.job.task;
 import org.job.ui.Mainwin;
 
 public class Boot {
-
 	public static void main(String[] args) {
 		//0.准备阶段；
 		CrawlerProcess crawlerProcess =new CrawlerProcess();
+		FServer server = new FServer(1233,"c:/");
 		//1.加载电报系统和办公系统的密码，弹出对话框并延时确认，
 		
 		//2.启动界面，加载数据库中默认的数据取出；
@@ -16,7 +16,8 @@ public class Boot {
 		crawlerProcess.addNotifyListener(window);
 		new Thread(crawlerProcess).start(); 
 		//3.启动轮训进程从网页上面取得数据；
-		new FServer(1233,"c:/").startfs();
+		server.startfs();
+		server.stopfs();
 		//4.通知界面要进行轮训；禁用
 		window.open();
 	}
