57506d9360ef5f1dffc7c9cfdd68a622850e7809
ebiskit <ebiskit@gmail.com>
2014-02-07 16:01:16 +0000

    BUG:修正JFTP的使用！

--- jobassistant2/src/org/job/task/FServer.java
+++ jobassistant2/src/org/job/task/FServer.java
@@ -9,46 +9,60 @@ import org.apache.ftpserver.listener.ListenerFactory;
 import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
 
 public class FServer {
+
+	private int port = 1234;
+	private String path = "c:/";
+	FtpServerFactory serverFactory = null;
+	ListenerFactory factory = null;
+	PropertiesUserManagerFactory userManagerFactory = null;
+	private FtpServer server = null;
+
 	public FServer(int port, String path) {
 		super();
 		this.port = port;
 		this.path = path;
-	}
-
-	private int port = 1234;
-	private String path ="c:/";
-	private FtpServer server = null;
-	
-	public void startfs(){
 		try {
-			FtpServerFactory serverFactory = new FtpServerFactory();
-			ListenerFactory factory = new ListenerFactory();
-			PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+			serverFactory = new FtpServerFactory();
+			factory = new ListenerFactory();
+			userManagerFactory = new PropertiesUserManagerFactory();
 			// set the port of the listener
 			factory.setPort(port);
 			// replace the default listener
 			serverFactory.addListener("default", factory.createListener());
-			userManagerFactory.setFile(new File("u.properties"));				//设置属性
+			userManagerFactory.setFile(new File("u.properties")); // 设置属性
 			userManagerFactory.setPath(path);
 			userManagerFactory.setUserpassword("");
-			serverFactory.setUserManager(userManagerFactory.createUserManager());	//使用属性建立用户
+			serverFactory
+					.setUserManager(userManagerFactory.createUserManager()); // 使用属性建立用户
 			// start the server
-			server = serverFactory.createServer();         
-			server.start();
-			//server.stop();
+			server = serverFactory.createServer();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void startfs() {
+		try {
+			if (server != null) {
+				server.start();
+			}
 		} catch (FtpException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void stopfs(){
-		if (server!=null) {
-			server.stop();	
+
+	public void stopfs() {
+		try {
+			if (server != null) {
+				server.stop();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		new FServer(1234,"c:/").startfs();
+		new FServer(1234, "c:/").startfs();
 	}
-	
+
 }
