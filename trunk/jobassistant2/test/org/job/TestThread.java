fc7f147b79b21ce6ce1bd4add705dda5cb460bdd
ebiskit@gmail.com <ebiskit@gmail.com@adcd3718-2b03-21e4-ee39-54743e69689c>
2014-02-08 15:55:45 +0000

    ADD:加入IM的客户端连接功能

---
+++ trunk/jobassistant2/test/org/job/TestThread.java
@@ -0,0 +1,28 @@
+package com.job;
+
+import org.hibernate.Session;
+import org.job.dao.HibernateSessionFactory;
+import org.job.dao.entity.DoccatalogDAO;
+
+
+public class TestThread {
+
+	public static void main(String[] args) {
+		
+		try{
+			new Thread(new WriteData()).start();
+			for (int i=99000;i<100000;i++){
+				Session session = HibernateUtil.currentSession();
+				session.beginTransaction();
+				DoccatalogDAO dao =new DoccatalogDAO();
+				System.out.println("read:"+i+":"+dao.findAll().size());
+				session.getTransaction().commit();
+				Thread.sleep(50);
+			}
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}finally{
+			HibernateUtil.closeSession();
+		}
+	}
+}
