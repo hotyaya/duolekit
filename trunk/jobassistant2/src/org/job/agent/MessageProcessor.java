03f95987ea3973ed5e5f28e890d3b3a66c7209bb
ebiskit@gmail.com <ebiskit@gmail.com@adcd3718-2b03-21e4-ee39-54743e69689c>
2014-02-16 14:28:46 +0000

    ADD:根据请求返回本机环境变更等等！

--- trunk/jobassistant2/src/org/job/agent/MessageProcessor.java
+++ trunk/jobassistant2/src/org/job/agent/MessageProcessor.java
@@ -1,9 +1,15 @@
 package org.job.agent;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.Properties;
+
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
+import org.job.Application;
 import org.job.task.FServer;
 
 public class MessageProcessor implements MessageListener {
@@ -22,13 +28,18 @@ public class MessageProcessor implements MessageListener {
 					server = new FServer(Integer.parseInt(arg1.getProperty("FPORT").toString().trim()),
 							arg1.getProperty("FDIR").toString().trim());
 					server.startfs();
-					chat.sendMessage("F-A-OK");
+					chat.sendMessage("F-A-OK\n");
 				}else if (cmd.equals("FO")){
 					server.stopfs();
 					server = null;
-					chat.sendMessage("F-O-OK");
-				}else if (cmd.equals(" ")){
-				
+					chat.sendMessage("F-O-OK\n");
+				}else if (cmd.equals("SYSPROP")){
+					try {
+						sendsysteminfo(chat);
+					} catch (UnknownHostException e) {
+						e.printStackTrace();
+					}
+					chat.sendMessage("SYSPROP-OK\n");
 				}else{
 					
 				}
@@ -41,4 +52,22 @@ public class MessageProcessor implements MessageListener {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	public void sendsysteminfo(Chat chat) throws UnknownHostException, XMPPException{
+		chat.sendMessage("HOSTADDRESS:/"+ InetAddress.getLocalHost().getHostAddress());
+		chat.sendMessage("HOSTNAME:/"+ InetAddress.getLocalHost().getHostName());
+		Message mesg = new Message();
+		Properties pros0 = Application.getPros();
+		Enumeration enu = pros0.keys();
+		while (enu.hasMoreElements()) {
+			String key=enu.nextElement().toString().trim();
+			mesg.setProperty(key, pros0.getProperty(key).toString().trim());
+			//System.out.println(":"+pros0.getProperty(key).toString().trim());
+		}
+		mesg.setBody("SYSTEM");
+		chat.sendMessage(mesg);
+	}
+	
+	
 }
